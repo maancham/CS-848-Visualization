d3ba668d263930142e26ef723d773ace25ba5338
maancham <houmaanch@gmail.com>
2023-10-12 19:30:13 -0300

    new layout

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function createBubbleChart(data, category) {
-  var width = 800;
-  var height = 800;
-  var centerX = width / 2;
-  var centerY = height / 2;
-  var backgroundCircleRadius = 250;
+  var screenWidth = window.innerWidth;
 
-  // Clear the previous SVG
+  var width = screenWidth * 0.7;
+  var height = 200;
+  var centerX = width / 5;
+
   d3.select("#bubble-chart").selectAll("svg").remove();
 
   var svg = d3.select("#bubble-chart").append("svg")
@@ -18,19 +17,21 @@ function createBubbleChart(data, category) {
 
   var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
+  var infoBox = document.getElementById("info-box");
+  var uniLogo = document.getElementById("uni-logo");
+  var uniName = document.getElementById("uni-name");
+  var uniLocation = document.getElementById("uni-location");
+  var uniFounded= document.getElementById("uni-founded");
+  var uniEnroll = document.getElementById("uni-enrollment");
+
   var circles = svg.selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
       .attr("cx", function(d, i) {
-        // Arrange circles on the perimeter of the background circle
-        var angle = (i / data.length) * 2 * Math.PI;
-        return centerX + backgroundCircleRadius * Math.cos(angle);
+        return centerX + i * 100; // Adjust the spacing between circles
       })
-      .attr("cy", function(d, i) {
-        var angle = (i / data.length) * 2 * Math.PI;
-        return centerY + backgroundCircleRadius * Math.sin(angle);
-      })
+      .attr("cy", height / 2)
       .attr("r", function(d) { return radiusScale(20 - 2*+d[category + "_ranking"]); })
       .attr("fill", function(d) { return colorScale(d.school); })
       .attr("class", "bubble")
@@ -49,38 +50,47 @@ function createBubbleChart(data, category) {
       })
       .on("mouseout", function() {
         // Restore the circle size
-        
         circles.transition().style("opacity", 1);
 
         d3.select(".tooltip").remove();
         d3.select(".tooltip")
           .style("display", "none");
+      })
+      .on("click", function(d) {
+        console.log(d.srcElement.__data__);
+        console.log(category);
+        uniLogo.src = d.srcElement.__data__.logo;
+        uniName.textContent = d.srcElement.__data__.school;
+        uniLocation.textContent = "Location: " + d.srcElement.__data__.location;
+        uniFounded.textContent = "Founded: " + d.srcElement.__data__.founded;
+        uniEnroll.textContent = "Head Count: " + d.srcElement.__data__.enrollment;
+  
       });
 
 
+  
   var labels = svg.selectAll("text")
       .data(data)
       .enter()
       .append("text")
       .attr("x", function(d, i) {
-        var angle = (i / data.length) * 2 * Math.PI;
-        return centerX + (backgroundCircleRadius + radiusScale(12 - 2*+d[category + "_ranking"])) * Math.cos(angle);
+        return centerX + i * 100; // Adjust the spacing between circles
       })
-      .attr("y", function(d, i) {
-        var angle = (i / data.length) * 2 * Math.PI;
-        return centerY + (backgroundCircleRadius + radiusScale(12 - 2*+d[category + "_ranking"])) * Math.sin(angle);
-      })
+      .attr("y", height / 2)
       .text(function(d) {
-        // Split the text into multiple lines
-        var words = d.school.split(" ");
-        return words.join("\n");
+        return d.school;
       })
       .attr("class", "label")
       .style("font-size", "10px")
       .style("text-anchor", "middle")
       .style("alignment-baseline", "middle")
       .style("pointer-events", "none");
+    
 }
+
+
+
+
 
 function updateVisualization(category) {
   d3.csv("top_canada.csv")
