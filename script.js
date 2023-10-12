f45715e53e5acb646a98b9b8a00d8b7650112998
maancham <houmaanch@gmail.com>
2023-10-12 05:16:51 -0300

    fix circle size issue

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function createBubbleChart(data, category) {
   var width = 800;
-  var height = 400;
+  var height = 800;
+  var centerX = width / 2;
+  var centerY = height / 2;
+  var backgroundCircleRadius = 250;
 
   // Clear the previous SVG
   d3.select("#bubble-chart").selectAll("svg").remove();
@@ -15,38 +18,78 @@ function createBubbleChart(data, category) {
 
   var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-  var bubbles = svg.selectAll("circle")
+  var circles = svg.selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
-      .attr("cx", function(d, i) { return (i % 5) * 150 + 100; }) // Adjust spacing and rows
-      .attr("cy", function(d, i) { return Math.floor(i / 5) * 100 + 100; }) // Adjust spacing and columns
-      .attr("r", function(d) { return radiusScale(+d[category + "_ranking"]); })
+      .attr("cx", function(d, i) {
+        // Arrange circles on the perimeter of the background circle
+        var angle = (i / data.length) * 2 * Math.PI;
+        return centerX + backgroundCircleRadius * Math.cos(angle);
+      })
+      .attr("cy", function(d, i) {
+        var angle = (i / data.length) * 2 * Math.PI;
+        return centerY + backgroundCircleRadius * Math.sin(angle);
+      })
+      .attr("r", function(d) { return radiusScale(20 - 2*+d[category + "_ranking"]); })
       .attr("fill", function(d) { return colorScale(d.school); })
       .attr("class", "bubble")
       .on("mouseover", function(event, d) {
-        var tooltipText = `School: ${d.school}\nRanking: ${d[category + "_ranking"]}\nCount: ${d[category + "_count"]}\nFaculty: ${d[category + "_faculty"]}`;
-  
-        d3.select("#tooltip")
-            .text(tooltipText)
-            .style("display", "block")
-            .style("left", (event.pageX + 10) + "px")
-            .style("top", (event.pageY - 30) + "px");
+        // Make the circle larger on hover
+        d3.select(this)
+          .transition()
+          .attr("r", 60);
+        // Others become grayish
+        circles.transition().style("opacity", 0.5);
+        var tooltip = d3.select("body").append("div")
+          .attr("class", "tooltip")
+          .style("display", "none");
+        d3.select(this)
+          .transition()
+          .attr("r", 60);
+
+        tooltip.html(
+          `<strong>${d.school}</strong><br>Ranking: ${d[category + "_ranking"]}<br>Count: ${d[category + "_count"]}<br>Faculty: ${d[category + "_faculty"]}`)
+          .style("display", "block")
+          .style("left", (event.pageX + 10) + "px")
+          .style("top", (event.pageY - 30) + "px");
       })
       .on("mouseout", function() {
-        d3.select("#tooltip").style("display", "none");
+        // Restore the circle size
+        d3.select(this)
+          .attr("r", function(d) { return radiusScale(12 - 2*+d[category + "_ranking"]); });
+        
+        circles.transition().style("opacity", 1);
+
+        d3.select(".tooltip").remove();
+        d3.select(".tooltip")
+          .style("display", "none");
       });
+
 
   var labels = svg.selectAll("text")
       .data(data)
       .enter()
       .append("text")
-      .attr("x", function(d, i) { return (i % 5) * 150 + 100; }) // Adjust spacing and rows
-      .attr("y", function(d, i) { return Math.floor(i / 5) * 100 + 100; }) // Adjust spacing and columns
-      .text(function(d) { return d.school; })
-      .attr("class", "label");
+      .attr("x", function(d, i) {
+        var angle = (i / data.length) * 2 * Math.PI;
+        return centerX + (backgroundCircleRadius + radiusScale(12 - 2*+d[category + "_ranking"])) * Math.cos(angle);
+      })
+      .attr("y", function(d, i) {
+        var angle = (i / data.length) * 2 * Math.PI;
+        return centerY + (backgroundCircleRadius + radiusScale(12 - 2*+d[category + "_ranking"])) * Math.sin(angle);
+      })
+      .text(function(d) {
+        // Split the text into multiple lines
+        var words = d.school.split(" ");
+        return words.join("\n");
+      })
+      .attr("class", "label")
+      .style("font-size", "10px")
+      .style("text-anchor", "middle")
+      .style("alignment-baseline", "middle")
+      .style("pointer-events", "none");
 }
-
 
 function updateVisualization(category) {
   d3.csv("top_canada.csv")
@@ -65,7 +108,6 @@ function updateVisualization(category) {
       var top10Universities = filteredData.slice(0, 10);
 
       createBubbleChart(top10Universities, category);
-
     })
     .catch(function (error) {
       console.error("Error loading the dataset:", error);
