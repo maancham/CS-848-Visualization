1ef816f55e25fde8a8c89cc69c2c2332c327941f
maancham <houmaanch@gmail.com>
2023-10-12 03:39:41 -0300

    circles in a row, first iterations

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-// Function to update the visualization based on the selected category
+function createBubbleChart(data, category) {
+  var width = 800;
+  var height = 400;
+
+  // Clear the previous SVG
+  d3.select("#bubble-chart").selectAll("svg").remove();
+
+  var svg = d3.select("#bubble-chart").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  var radiusScale = d3.scaleSqrt()
+      .domain([0, d3.max(data, function(d) { return +d[category + "_ranking"]; })])
+      .range([10, 40]);
+
+  var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+  var bubbles = svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d, i) { return (i % 5) * 150 + 100; }) // Adjust spacing and rows
+      .attr("cy", function(d, i) { return Math.floor(i / 5) * 100 + 100; }) // Adjust spacing and columns
+      .attr("r", function(d) { return radiusScale(+d[category + "_ranking"]); })
+      .attr("fill", function(d) { return colorScale(d.school); })
+      .attr("class", "bubble")
+      .on("mouseover", function(event, d) {
+        var tooltipText = `School: ${d.school}\nRanking: ${d[category + "_ranking"]}\nCount: ${d[category + "_count"]}\nFaculty: ${d[category + "_faculty"]}`;
+  
+        d3.select("#tooltip")
+            .text(tooltipText)
+            .style("display", "block")
+            .style("left", (event.pageX + 10) + "px")
+            .style("top", (event.pageY - 30) + "px");
+      })
+      .on("mouseout", function() {
+        d3.select("#tooltip").style("display", "none");
+      });
+
+  var labels = svg.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("x", function(d, i) { return (i % 5) * 150 + 100; }) // Adjust spacing and rows
+      .attr("y", function(d, i) { return Math.floor(i / 5) * 100 + 100; }) // Adjust spacing and columns
+      .text(function(d) { return d.school; })
+      .attr("class", "label");
+}
+
+
 function updateVisualization(category) {
   d3.csv("top_canada.csv")
     .then(function (data) {
@@ -15,22 +64,8 @@ function updateVisualization(category) {
 
       var top10Universities = filteredData.slice(0, 10);
 
-      var visualization = document.getElementById("visualization");
-      visualization.innerHTML = "";
+      createBubbleChart(top10Universities, category);
 
-      top10Universities.forEach(function (university) {
-        var universityElement = document.createElement("div");
-        universityElement.classList.add("university");
-        universityElement.innerHTML = `
-            <div class="university-name">${university.school} - Ranking: ${
-          university[category + "_ranking"]
-        }</div>
-            <div class="category-info">Count: ${
-              university[category + "_count"]
-            } - Faculty: ${university[category + "_faculty"]}</div>
-        `;
-        visualization.appendChild(universityElement);
-      });
     })
     .catch(function (error) {
       console.error("Error loading the dataset:", error);
